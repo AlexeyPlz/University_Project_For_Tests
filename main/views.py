b89d0928deb7a7e590c486ae0717e304d89067b5
PolinaP24 <petrovapolina24@mail.ru>
2023-06-03 02:26:16 +0400

    added data for test

--- main/views.py
+++ main/views.py
@@ -1,12 +1,10 @@
-from datetime import datetime
 from django.contrib.auth import logout, login
 from django.contrib.auth.views import LoginView
-from django.shortcuts import redirect
 from django.urls import reverse_lazy
 from django.views.generic import CreateView, DetailView, ListView
 
-from .forms import LoginForm, RegistrationForm, ResultForm
-from .models import Answer, Result, Test
+from .forms import LoginForm, RegistrationForm
+from .models import Result, Test
 
 
 class Login(LoginView):
@@ -25,45 +23,34 @@ class Registration(CreateView):
     def form_valid(self, form):
         user = form.save()
         login(self.request, user)
-        return redirect('main:main')
 
 
 class Main(ListView):
     model = Test
-    template_name = 'main/main.html'
-    context_object_name = 'tests'
+    template_name = "main/main.html"
+
+    def get_queryset(self):
+        return super().get_queryset().prefetch_related('author', 'tasks')
 
 
 class Test(DetailView):
     model = Test
-    template_name = 'main/test.html'
+    template_name = "main/test.html"
 
 
 class Profile(ListView):
     model = Result
-    template_name = 'main/profile.html'
-    context_object_name = 'results'
+    template_name = "main/profile.html"
 
     def get_queryset(self):
-        return super().get_queryset().filter(student__id=self.request.user.id)
+        users = super().get_queryset()
+        return users.filter(student__id=self.request.user.id).select_related('student', 'test')
 
 
 def logout_user(request):
     logout(request)
-    return redirect('main:main')
+    return reverse_lazy('main:main')
 
 
-def result(request):
-    if request.method == 'POST':
-        form = ResultForm(request.POST)
-        if form.is_valid():
-            result = form.save(commit=False)
-            result.points = 0
-            for answer in result.answers:
-                answer = Answer.objects.get(id=answer.get('answer_id'))
-                if answer.is_right:
-                    result.points += answer.point
-            result.date = datetime.now()
-            result.student = request.user
-            result.save()
-    return redirect('main:main')
+def result(self, request):
+    pass
